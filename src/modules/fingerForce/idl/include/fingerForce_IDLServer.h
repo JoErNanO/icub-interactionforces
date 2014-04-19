dc5ebc092090c7f1ce78835eb4e7b2e410eed88c
Francesco Giovannini <francesco.giovannini@iit.it>
2014-04-19 16:38:02 +0200

    [IDL] Building idl files into idl directory.

---
+++ src/modules/fingerForce/idl/include/fingerForce_IDLServer.h
@@ -0,0 +1,46 @@
+// This is an automatically-generated file.
+// It could get re-generated if the ALLOW_IDL_GENERATION flag is on.
+
+#ifndef YARP_THRIFT_GENERATOR_fingerForce_IDLServer
+#define YARP_THRIFT_GENERATOR_fingerForce_IDLServer
+
+#include <yarp/os/Wire.h>
+#include <yarp/os/idl/WireTypes.h>
+
+class fingerForce_IDLServer;
+
+
+/**
+ * fingerForce_IDLServer
+ * IDL Interface to \ref fingerForce services.
+ */
+class fingerForce_IDLServer : public yarp::os::Wire {
+public:
+  fingerForce_IDLServer() { yarp().setOwner(*this); }
+/**
+ * Opens the robot hand.
+ * @return true/false on success/failure
+ */
+  virtual bool open();
+/**
+ * Grasp an object using a pinch grasp.
+ * The grasping movement is controlled in position mode.
+ * @return true/false on success/failure.
+ */
+  virtual bool pinch();
+/**
+ * Perform a sequence of pinch grasps.
+ * @return true/false on success/failure.
+ */
+  virtual bool pinchseq();
+/**
+ * Quit the module.
+ * @return true/false on success/failure.
+ */
+  virtual bool quit();
+  virtual bool read(yarp::os::ConnectionReader& connection);
+  virtual std::vector<std::string> help(const std::string& functionName="--all");
+};
+
+#endif
+
---
+++ src/modules/fingerForce/idl/src/fingerForce_IDLServer.cpp
@@ -0,0 +1,250 @@
+// This is an automatically-generated file.
+// It could get re-generated if the ALLOW_IDL_GENERATION flag is on.
+
+#include <fingerForce_IDLServer.h>
+#include <yarp/os/idl/WireTypes.h>
+
+
+
+class fingerForce_IDLServer_open : public yarp::os::Portable {
+public:
+  bool _return;
+  virtual bool write(yarp::os::ConnectionWriter& connection) {
+    yarp::os::idl::WireWriter writer(connection);
+    if (!writer.writeListHeader(1)) return false;
+    if (!writer.writeTag("open",1,1)) return false;
+    return true;
+  }
+  virtual bool read(yarp::os::ConnectionReader& connection) {
+    yarp::os::idl::WireReader reader(connection);
+    if (!reader.readListReturn()) return false;
+    if (!reader.readBool(_return)) {
+      reader.fail();
+      return false;
+    }
+    return true;
+  }
+};
+
+class fingerForce_IDLServer_pinch : public yarp::os::Portable {
+public:
+  bool _return;
+  virtual bool write(yarp::os::ConnectionWriter& connection) {
+    yarp::os::idl::WireWriter writer(connection);
+    if (!writer.writeListHeader(1)) return false;
+    if (!writer.writeTag("pinch",1,1)) return false;
+    return true;
+  }
+  virtual bool read(yarp::os::ConnectionReader& connection) {
+    yarp::os::idl::WireReader reader(connection);
+    if (!reader.readListReturn()) return false;
+    if (!reader.readBool(_return)) {
+      reader.fail();
+      return false;
+    }
+    return true;
+  }
+};
+
+class fingerForce_IDLServer_pinchseq : public yarp::os::Portable {
+public:
+  bool _return;
+  virtual bool write(yarp::os::ConnectionWriter& connection) {
+    yarp::os::idl::WireWriter writer(connection);
+    if (!writer.writeListHeader(1)) return false;
+    if (!writer.writeTag("pinchseq",1,1)) return false;
+    return true;
+  }
+  virtual bool read(yarp::os::ConnectionReader& connection) {
+    yarp::os::idl::WireReader reader(connection);
+    if (!reader.readListReturn()) return false;
+    if (!reader.readBool(_return)) {
+      reader.fail();
+      return false;
+    }
+    return true;
+  }
+};
+
+class fingerForce_IDLServer_quit : public yarp::os::Portable {
+public:
+  bool _return;
+  virtual bool write(yarp::os::ConnectionWriter& connection) {
+    yarp::os::idl::WireWriter writer(connection);
+    if (!writer.writeListHeader(1)) return false;
+    if (!writer.writeTag("quit",1,1)) return false;
+    return true;
+  }
+  virtual bool read(yarp::os::ConnectionReader& connection) {
+    yarp::os::idl::WireReader reader(connection);
+    if (!reader.readListReturn()) return false;
+    if (!reader.readBool(_return)) {
+      reader.fail();
+      return false;
+    }
+    return true;
+  }
+};
+
+bool fingerForce_IDLServer::open() {
+  bool _return = false;
+  fingerForce_IDLServer_open helper;
+  if (!yarp().canWrite()) {
+    fprintf(stderr,"Missing server method '%s'?\n","bool fingerForce_IDLServer::open()");
+  }
+  bool ok = yarp().write(helper,helper);
+  return ok?helper._return:_return;
+}
+bool fingerForce_IDLServer::pinch() {
+  bool _return = false;
+  fingerForce_IDLServer_pinch helper;
+  if (!yarp().canWrite()) {
+    fprintf(stderr,"Missing server method '%s'?\n","bool fingerForce_IDLServer::pinch()");
+  }
+  bool ok = yarp().write(helper,helper);
+  return ok?helper._return:_return;
+}
+bool fingerForce_IDLServer::pinchseq() {
+  bool _return = false;
+  fingerForce_IDLServer_pinchseq helper;
+  if (!yarp().canWrite()) {
+    fprintf(stderr,"Missing server method '%s'?\n","bool fingerForce_IDLServer::pinchseq()");
+  }
+  bool ok = yarp().write(helper,helper);
+  return ok?helper._return:_return;
+}
+bool fingerForce_IDLServer::quit() {
+  bool _return = false;
+  fingerForce_IDLServer_quit helper;
+  if (!yarp().canWrite()) {
+    fprintf(stderr,"Missing server method '%s'?\n","bool fingerForce_IDLServer::quit()");
+  }
+  bool ok = yarp().write(helper,helper);
+  return ok?helper._return:_return;
+}
+
+bool fingerForce_IDLServer::read(yarp::os::ConnectionReader& connection) {
+  yarp::os::idl::WireReader reader(connection);
+  reader.expectAccept();
+  if (!reader.readListHeader()) { reader.fail(); return false; }
+  yarp::os::ConstString tag = reader.readTag();
+  while (!reader.isError()) {
+    // TODO: use quick lookup, this is just a test
+    if (tag == "open") {
+      bool _return;
+      _return = open();
+      yarp::os::idl::WireWriter writer(reader);
+      if (!writer.isNull()) {
+        if (!writer.writeListHeader(1)) return false;
+        if (!writer.writeBool(_return)) return false;
+      }
+      reader.accept();
+      return true;
+    }
+    if (tag == "pinch") {
+      bool _return;
+      _return = pinch();
+      yarp::os::idl::WireWriter writer(reader);
+      if (!writer.isNull()) {
+        if (!writer.writeListHeader(1)) return false;
+        if (!writer.writeBool(_return)) return false;
+      }
+      reader.accept();
+      return true;
+    }
+    if (tag == "pinchseq") {
+      bool _return;
+      _return = pinchseq();
+      yarp::os::idl::WireWriter writer(reader);
+      if (!writer.isNull()) {
+        if (!writer.writeListHeader(1)) return false;
+        if (!writer.writeBool(_return)) return false;
+      }
+      reader.accept();
+      return true;
+    }
+    if (tag == "quit") {
+      bool _return;
+      _return = quit();
+      yarp::os::idl::WireWriter writer(reader);
+      if (!writer.isNull()) {
+        if (!writer.writeListHeader(1)) return false;
+        if (!writer.writeBool(_return)) return false;
+      }
+      reader.accept();
+      return true;
+    }
+    if (tag == "help") {
+      std::string functionName;
+      if (!reader.readString(functionName)) {
+        functionName = "--all";
+      }
+      std::vector<std::string> _return=help(functionName);
+      yarp::os::idl::WireWriter writer(reader);
+        if (!writer.isNull()) {
+          if (!writer.writeListHeader(2)) return false;
+          if (!writer.writeTag("many",1, 0)) return false;
+          if (!writer.writeListBegin(BOTTLE_TAG_INT, static_cast<uint32_t>(_return.size()))) return false;
+          std::vector<std::string> ::iterator _iterHelp;
+          for (_iterHelp = _return.begin(); _iterHelp != _return.end(); ++_iterHelp)
+          {
+            if (!writer.writeString(*_iterHelp)) return false;
+           }
+          if (!writer.writeListEnd()) return false;
+        }
+      reader.accept();
+      return true;
+    }
+    if (reader.noMore()) { reader.fail(); return false; }
+    yarp::os::ConstString next_tag = reader.readTag();
+    if (next_tag=="") break;
+    tag = tag + "_" + next_tag;
+  }
+  return false;
+}
+
+std::vector<std::string> fingerForce_IDLServer::help(const std::string& functionName) {
+  bool showAll=(functionName=="--all");
+  std::vector<std::string> helpString;
+  if(showAll) {
+    helpString.push_back("*** Available commands:");
+    helpString.push_back("open");
+    helpString.push_back("pinch");
+    helpString.push_back("pinchseq");
+    helpString.push_back("quit");
+    helpString.push_back("help");
+  }
+  else {
+    if (functionName=="open") {
+      helpString.push_back("bool open() ");
+      helpString.push_back("Opens the robot hand. ");
+      helpString.push_back("@return true/false on success/failure ");
+    }
+    if (functionName=="pinch") {
+      helpString.push_back("bool pinch() ");
+      helpString.push_back("Grasp an object using a pinch grasp. ");
+      helpString.push_back("The grasping movement is controlled in position mode. ");
+      helpString.push_back("@return true/false on success/failure. ");
+    }
+    if (functionName=="pinchseq") {
+      helpString.push_back("bool pinchseq() ");
+      helpString.push_back("Perform a sequence of pinch grasps. ");
+      helpString.push_back("@return true/false on success/failure. ");
+    }
+    if (functionName=="quit") {
+      helpString.push_back("bool quit() ");
+      helpString.push_back("Quit the module. ");
+      helpString.push_back("@return true/false on success/failure. ");
+    }
+    if (functionName=="help") {
+      helpString.push_back("std::vector<std::string> help(const std::string& functionName=\"--all\")");
+      helpString.push_back("Return list of available commands, or help message for a specific function");
+      helpString.push_back("@param functionName name of command for which to get a detailed description. If none or '--all' is provided, print list of available commands");
+      helpString.push_back("@return list of strings (one string per line)");
+    }
+  }
+  if ( helpString.empty()) helpString.push_back("Command not found");
+  return helpString;
+}
+
+
